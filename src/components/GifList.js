95c7752357cc2a8ed7c62314705be83b97f9cb15
clarkj99 <clark@stontech.net>
2019-12-12 00:55:08 -0500

    image border

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,7 +3,7 @@ import React from "react"
 function GifList(props) {
     return (
         <ul className="list-unstyled">
-            {props.gifList.map((gif) => <li key={gif.id}> <img className="img-responsive" src={gif.images.original.url} /> </li>)}
+            {props.gifList.map((gif) => <li key={gif.id}> <img className="img-responsive img-thumbnail" src={gif.images.original.url} /> </li>)}
         </ul>
     )
 }
